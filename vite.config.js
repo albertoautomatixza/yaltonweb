0e721198ee854492bca1b65794c00030ce45f5ab
albertoautomatixza <licbetoacostareyes@gmail.com>
2026-03-24 19:34:41 -0600

    Update Oportunidades to Promociones

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         empresa: resolve(__dirname, 'empresa.html'),
-        oportunidades: resolve(__dirname, 'oportunidades.html')
+        promociones: resolve(__dirname, 'promociones.html')
       }
     }
   }
